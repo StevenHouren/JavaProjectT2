6797d3672d36f307bf16325f39522e1e90742621
SteveHouren <shouren@umich.edu>
2018-10-28 19:51:17 -0400

    Initialization commit

---
+++ src/main/java/com/jtp1/Jtp1Application.java
@@ -0,0 +1,12 @@
+package com.jtp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jtp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jtp1Application.class, args);
+    }
+}
